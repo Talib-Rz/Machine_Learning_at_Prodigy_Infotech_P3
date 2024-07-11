7e33d566ba64d067c516cd3259466b4029419f9a
Talib_Rz <133497443+Talib-Rz@users.noreply.github.com>
2024-07-12 02:52:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,82 +3,65 @@ from PIL import Image
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, save_img
 from tensorflow.keras.models import model_from_json
 import numpy as np
+import tempfile
 import shutil
 
-import os # inbuilt module
-import random # inbuilt module
-import webbrowser # inbuilt module
+# Model loading (assuming model files are in "model" directory)
+model_path_h5 = "model/model.h5"
+model_path_json = "model/model.json"
+json_file = open(model_path_json, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights(model_path_h5)
+
+loaded_model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
 
 st.title("""
 Cat or Dog Recognizer
 	""")
 
-index = random.choice([0,1])
-image = Image.open(img_path_list[index])
-st.image(
-	        image,
-	        use_column_width=True,
-	    )
-
 img_file_buffer = st.file_uploader("Please! Upload here:")
 
 try:
-	image = Image.open(img_file_buffer)
-	img_array = np.array(image)
-	if image is not None:
-	    st.image(
-	        image,
-	        use_column_width=True
-	    )
+  image = Image.open(img_file_buffer)
+  img_array = np.array(image)
+  if image is not None:
+    st.image(
+      image,
+      use_column_width=True
+    )
 except:
-	st.write("""
-		    Any Picture hasn't selected yet!!!
-		""")
+  st.write("""
+      Any Picture hasn't selected yet!!!
+      """)
 
 st.text("""""")
 submit = st.button("Predict")
 
+
 def processing(testing_image_path):
-    IMG_SIZE = 50
-    img = load_img(testing_image_path, 
-    target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
-    img_array = img_to_array(img)
-    img_array = img_array/255.0
-    img_array = img_array.reshape((1, 50, 50, 1))   
-    prediction =loaded_model.predict(img_array)    
-    return prediction
+  IMG_SIZE = 50
+  img = load_img(testing_image_path, target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
+  img_array = img_to_array(img)
+  img_array = img_array / 255.0
+  img_array = img_array.reshape((1, 50, 50, 1))
+  prediction = loaded_model.predict(img_array)
+  return prediction
+
 
 def generate_result(prediction):
-	if prediction[0]<0.5:
-	    st.write("""
-	    	Model predicted it as an image of a CAT
-	    	""")
-	else:
-	    st.write("""
-	    	Model predicted it as an image of a DOG
-	    	""")
+  if prediction[0] < 0.5:
+    st.write("""
+      Model predicted it as an image of a CAT
+      """)
+  else:
+    st.write("""
+      Model predicted it as an image of a DOG
+      """)
+
 
 if submit:
-	try:
-		# save image on that directory
-		save_img("temp_dir/test_image.png", img_array)
-
-		image_path = "temp_dir/test_image.png"
-		model_path_h5 = "model/model.h5"
-		model_path_json = "model/model.json"
-		json_file = open(model_path_json, 'r')
-		loaded_model_json = json_file.read()
-		json_file.close()
-		loaded_model = model_from_json(loaded_model_json)
-		loaded_model.load_weights(model_path_h5)
-
-		loaded_model.compile(loss='binary_crossentropy', metrics=['accuracy'],optimizer='adam')
-
-		prediction = processing(image_path)
-
-		generate_result(prediction)
-
-	except:
-		st.write("""
-		        Lunch Time! Please visit later, Thank you :)
-			""")
+  try:
+    # Create temporary directory
+    temp_dir = tempfile.mkdtemp
