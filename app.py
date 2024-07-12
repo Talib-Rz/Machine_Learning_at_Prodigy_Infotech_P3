2c0f1938c8eaebab202dd802455777c0e150374f
Talib_Rz <133497443+Talib-Rz@users.noreply.github.com>
2024-07-12 16:13:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,81 +3,60 @@ from PIL import Image
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, save_img
 from tensorflow.keras.models import model_from_json
 import numpy as np
-import tempfile
-import shutil
+import random
 
-# Model loading (assuming model files are in "model" directory)
-model_path_h5 = "model/model.h5"
-model_path_json = "model/model.json"
-json_file = open(model_path_json, 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights(model_path_h5)
+# Title
+st.title("Cat 🐱 Or Dog 🐶 Recognizer")
 
-loaded_model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-st.title("""
-Cat or Dog Recognizer
-	""")
-
-img_file_buffer = st.file_uploader("Please! Upload here:")
+# File Uploader
+img_file_buffer = st.file_uploader("Upload an image here 👇🏻")
 
 try:
-  image = Image.open(img_file_buffer)
-  img_array = np.array(image)
-  if image is not None:
-    st.image(
-      image,
-      use_column_width=True
-    )
+    image = Image.open(img_file_buffer)
+    img_array = np.array(image)
+    st.image(image, use_column_width=True)
+    st.write("Now, click the '👉🏼 Predict' button to see the prediction!")
 except:
-  st.write("""
-      Any Picture hasn't selected yet!!!
-      """)
+    st.write("Any Picture hasn't selected yet!")
 
-st.text("""""")
-submit = st.button("Predict")
+# Predict Button
+submit = st.button("👉🏼 Predict")
 
-
+# Model Prediction Function
 def processing(testing_image_path):
-  IMG_SIZE = 50
-  img = load_img(testing_image_path, target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
-  img_array = img_to_array(img)
-  img_array = img_array / 255.0
-  img_array = img_array.reshape((1, 50, 50, 1))
-  prediction = loaded_model.predict(img_array)
-  return prediction
+    IMG_SIZE = 50
+    img = load_img(testing_image_path, target_size=(IMG_SIZE, IMG_SIZE), color_mode="grayscale")
+    img_array = img_to_array(img) / 255.0
+    img_array = img_array.reshape((1, 50, 50, 1))
+    prediction = loaded_model.predict(img_array)
+    return prediction
 
-
+# Generate Result Function
 def generate_result(prediction):
-  if prediction[0] < 0.5:
-    st.write("""
-      Model predicted it as an image of a CAT
-      """)
-  else:
-    st.write("""
-      Model predicted it as an image of a DOG
-      """)
+    st.write("## 🎯 RESULT")
+    if prediction[0] < 0.5:
+        st.write("## Model predicts it as an image of a CAT 🐱!!!")
+    else:
+        st.write("## Model predicts it as an image of a DOG 🐶!!!")
 
-
+# Predict Button Clicked
 if submit:
+    try:
+        save_img("temp_dir/test_image.png", img_array)
+        image_path = "temp_dir/test_image.png"
 
-    # save image on that directory
-    save_img("static/uploads/test_image.png", img_array)
+        # Load Model
+        with open("model/model.json", 'r') as json_file:
+            loaded_model_json = json_file.read()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights("model/model.h5")
+        loaded_model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-    image_path = "static/uploads/test_image.png"
-    # Predicting
-    st.write("👁️ Predicting...")
+        prediction = processing(image_path)
+        generate_result(prediction)
+    except:
+        st.write("Oops... Something is wrong")
 
-    loaded_model = load_model("static/models/model.h5")
-
-    prediction = processing(image_path)
-
-    cat_value = prediction[0][0] * 100
-    dog_value = prediction[0][1] * 100
-
-    generate_result(prediction)
-
-    st.write("Chance of Cat: ", cat_value, " %")
-    st.write("Chance of Dog: ", dog_value, " %")
+# Footer
+st.write("Cooked By Talib Rz")
